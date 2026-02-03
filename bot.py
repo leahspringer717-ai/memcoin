f2822c67c220feb1762ba3e4f20bb9d7dfddb0b3
leahspringer717-ai <leahspringer717@gmail.com>
2026-02-03 23:35:02 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,12 +8,7 @@ from config import BOT_TOKEN, CHECK_INTERVAL, ALERT_THRESHOLD
 from dex import get_usdt_pair
 from analyzer import anti_rug_score, is_pump
 from storage import coins, last_alert
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-
-if not BOT_TOKEN:
-    raise ValueError("BOT_TOKEN не задан в переменных окружения")
-
-BOT_TOKEN = BOT_TOKEN.strip()  # 🔥 ВАЖНО
+BOT_TOKEN = os.getenv("BOT_TOKEN").strip()
 bot = telebot.TeleBot(BOT_TOKEN, parse_mode="Markdown")
 
 DEX_REGEX = r"0x[a-fA-F0-9]{40}"
