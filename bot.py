ccd68d65177ace51d986c5d06a6437decac101e9
leahspringer717-ai <leahspringer717@gmail.com>
2026-02-03 23:38:12 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import os
+
 import telebot
 import re
 import time
@@ -8,7 +8,7 @@ from config import BOT_TOKEN, CHECK_INTERVAL, ALERT_THRESHOLD
 from dex import get_usdt_pair
 from analyzer import anti_rug_score, is_pump
 from storage import coins, last_alert
-BOT_TOKEN = os.getenv("BOT_TOKEN").strip()
+
 bot = telebot.TeleBot(BOT_TOKEN, parse_mode="Markdown")
 
 DEX_REGEX = r"0x[a-fA-F0-9]{40}"
